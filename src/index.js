eeffab46e18cd367b893535fb5dde6ed140fa46c
izaRibeiro <bellarpjf@gmail.com>
2022-02-10 00:54:51 -0300

    :wrench: adiciona configurações iniciais do swagger

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import employeeRouter from './routes/employee.route.js'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 const app = express()
 dotenv.config()
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 mongoose.connect(
     `${process.env.MONGO_URL}`, {
