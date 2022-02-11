18abf63fd80b135afc0812ce7c31b6c9d1ed7c77
izaRibeiro <bellarpjf@gmail.com>
2022-02-11 16:11:25 -0300

    :lock: insere autenticação no swagger

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -5,6 +5,7 @@ export default async(req, res, next) => {
     if (!email) res.status(401).send({ error: "Usuário não autorizado" })
 
     const employee = await employeeRepository.getEmployeeByEmail(email)
+    if (!employee) res.status(404).send({ error: "Usuário não encontrado" })
     if (employee.type != 'admin') res.status(401).send({ error: "Usuário não autorizado" })
     return next()
 }
